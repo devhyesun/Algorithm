5d47f595a8f03934db53629b72c301bf53fe725a
hyesun shin <yn6201@naver.com>
2020-04-07 21:07:05 +0900

    [LeetCode] 326

---
+++ LeetCode/326/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int input = sc.nextInt();
+		
+		Solution s = new Solution();
+		System.out.println(s.isPowerOfThree(input));
+	}
+
+}
