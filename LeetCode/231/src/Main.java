07ea208ed366d0b0ded82a919facc7bbb17229dc
hyesun shin <yn6201@naver.com>
2020-04-12 22:23:40 +0900

    [LeetCode] 231

---
+++ LeetCode/231/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Solution s = new Solution();
+		
+		System.out.println(s.isPowerOfTwo(0));
+		System.out.println(s.isPowerOfTwo(1));
+		System.out.println(s.isPowerOfTwo(16));
+		System.out.println(s.isPowerOfTwo(218));
+	}
+
+}
---
+++ LeetCode/231/src/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        
+        while(n > 1) {
+        	if(n%2 != 0) {
+        		return false;
+        	}
+        	
+        	n /= 2;
+        }
+        
+        return n == 1;
+    }
+}	
